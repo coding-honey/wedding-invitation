63dc8124f9e66031b071cc5cef527c00cd3829f7
ehdgjsdl1 <ehdgjsdl1@naver.com>
2024-03-24 23:13:16 +0900

    Fix: naver map 좌표 위치 수정

--- app/ui/NaverMap.tsx
+++ app/ui/NaverMap.tsx
@@ -4,16 +4,17 @@ import Script from "next/script";
 
 export default function NaverMap() {
   const initMap = () => {
-  // Brewery
-  var brewery = new naver.maps.Map('map', {
-    center: new naver.maps.LatLng(37.501860, 127.066003),
-    zoom: 15,
-    zoomControl: true
-  });
-  var breweryMarker = new naver.maps.Marker({
-    position: new naver.maps.LatLng(37.501860, 127.066003),
-    map: brewery
-  })};
+    // Brewery
+    var map = new naver.maps.Map('map', {
+      center: new naver.maps.LatLng(37.501817, 127.066025),
+      zoom: 15,
+      zoomControl: true
+    });
+    var breweryMarker = new naver.maps.Marker({
+      position: new naver.maps.LatLng(37.501817, 127.066025),
+      map: map
+    })
+  };
   //useEffect hook을 사용하여 초기 페이지가 로딩될때 맵을 생성합니다.
   useEffect(() => {
     initMap()
