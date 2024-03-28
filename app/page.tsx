276ebb4ce588d2b8616a835653932fb41b4bdb59
ehdgjsdl1 <ehdgjsdl1@naver.com>
2024-03-28 23:38:08 +0900

    Feat: 참석 의사 전달하기 Modal 추가

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import ImageSlider from "@/app/ui/ImageSlider";
 import dynamic from "next/dynamic";
 import CalendarWrapper from "@/app/ui/CalendarWrapper";
 import NaverMap from "@/app/ui/component/NaverMap";
+import AttendanceModal from "@/app/ui/AttendanceModal";
 
 const VideoPlayer = dynamic(() => import("@/app/ui/component/VideoPlayer"), { ssr: false });
 
@@ -47,22 +48,22 @@ export default function Home() {
       </section>
       <section className="fs-4 mx-4">
         <div className="row mb-0">
-          <div className="col col-md col-sm text-end">
+          <div className="col text-end">
             <span>정석순</span>
             <span className="mx-2"></span>
             <span>김형순</span>
           </div>
-          <div className="col col-md col-sm text-start">
+          <div className="col text-start">
             <span className="fs-6">장남</span>
             <span className="mx-2"></span>
             <span>정동헌</span>
           </div>
         </div>
         <div className="row">
-          <div className="col col-md col-sm text-end">
+          <div className="col text-end">
             <span>김남희</span>
           </div>
-          <div className="col col-md col-sm text-start">
+          <div className="col text-start">
             <span className="fs-6">장녀</span>
             <span className="mx-2"></span>
             <span>박선영</span>
@@ -98,7 +99,7 @@ export default function Home() {
           </div>
         </div>
         <div className="row mb-1 mb-3 align-items-center">
-          <div className="col-5 text-end">신랑 어머지</div>
+          <div className="col-5 text-end">신랑 어머니</div>
           <div className="col-3">김형순</div>
           <div className="col-4 d-flex justify-content-start align-items-center">
             <a className="tel" href="tel:01076715530">
@@ -161,22 +162,40 @@ export default function Home() {
       <section className="text-start mx-5">
         <div className="fs-2 fw-bold">지하철</div>
         <div>
-          <span className="fs-4">[2호선] 삼성역 3번출구 도보 14분</span>
-          <br/>
-          <span className="fs-4">[3호선] 학여울역 1번출구 도보 14분</span>
+          <div className="fs-4">[2호선] 삼성역 3번출구 도보 14분</div>
+          <div className="fs-4">[3호선] 학여울역 1번출구 도보 14분</div>
         </div>
         <div className="fs-2 fw-bold">버스</div>
         <div>
-          <span className="fs-4">마을버스 강남 01, 강남 06</span>
-          <br/>
-          <span className="fs-4">401, 4319, 4419, 500-2, 9507, 9607, 11-3, 917</span>
+          <div className="fs-4">마을버스 강남 01, 강남 06</div>
+          <div className="fs-4">401, 4319, 4419, 500-2, 9507, 9607, 11-3, 917</div>
         </div>
         <div className="fs-2 fw-bold">주차정보</div>
         <div>
-          <span className="fs-4">* 대치2동문화센터 공영주차장입구</span>
-          <br/>
-          <span className="fs-4">주소: 서울 강남구 영동대로65길 24 대치2동주민센터</span>
+          <div className="fs-4">* 대치2동문화센터 공영주차장입구</div>
+          <div className="fs-4">주소: 서울 강남구 영동대로65길 24 대치2동주민센터</div>
         </div>
+      </section>
+      <section>
+        <Image
+          className="img-fluid"
+          src="/main3.png"
+          width={439}
+          height={645}
+          alt="main picture 3">
+        </Image>
+      </section>
+      <section>
+        <div className="fs-2 fw-bold">참석 의사 전달</div>
+        <div className="fs-4">
+          <div>축하의 마음으로 참석해주시는</div>
+          <div>모든 분들을 귀하게 모실 수 있도록</div>
+          <div>참석 의사를 전달 부탁드립니다.</div>
+        </div>
+        <button type="button" className="btn btn-lg btn-dark w-75 fs-4">참석 의사 전달하기</button>
+      </section>
+      <section className="border">
+        <AttendanceModal />
       </section>
     </main>
   );
