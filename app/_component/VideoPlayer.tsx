7a202065a503c0fecd184afa4d5dbf1408ac9c9c
ehdgjsdl1 <ehdgjsdl1@naver.com>
2024-10-05 19:00:53 +0900

    Feat: Video 실제 영상으로 교체

--- app/_component/VideoPlayer.tsx
+++ app/_component/VideoPlayer.tsx
@@ -1,11 +1,14 @@
 "use client";
-import ReactPlayer from 'react-player'; // https://www.npmjs.com/package/react-player#props
+
+import ReactPlayer from 'react-player';
 
 export default function VideoPlayer() {
   return (
     <ReactPlayer
-      url="https://youtu.be/0ZgVQJ2gAjE?si=CpVpCcoZ7492Kdi6"
-      width="100%"
+      className="img-fluid"
+      url="/video/wedding-video.mp4"
+      height={190}
+      controls
     />
   );
 }
