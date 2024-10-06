96c364c4d87c376a192ac2acf4aee3bfd957c5ce
ehdgjsdl1 <ehdgjsdl1@naver.com>
2024-10-07 01:34:20 +0900

    Refactor: Alert severity 속성 추가

--- app/_component/alert/AlertGlobal.tsx
+++ app/_component/alert/AlertGlobal.tsx
@@ -5,14 +5,14 @@ import {Alert} from "@mui/material";
 import {useAlert} from "@/app/_provider/AlertProvider";
 
 export default function AlertGlobal() {
-  const {title, isAlertOpen, closeAlert} = useAlert();
+  const {title, severity, isAlertOpen, closeAlert} = useAlert();
   if (!isAlertOpen) return null;
 
   // Modal을 body에 렌더링
   return createPortal(
     <div className="modal-overlay" onClick={closeAlert}>
       <div className="modal-content pb-0" onClick={(e) => e.stopPropagation()}>
-        <Alert severity="success">{title}</Alert>
+        <Alert severity={severity}>{title}</Alert>
       </div>
     </div>,
     document.body
--- app/_provider/AlertProvider.tsx
+++ app/_provider/AlertProvider.tsx
@@ -1,16 +1,22 @@
 "use client";
 
 import {createContext, useContext, useMemo, useState} from "react";
+import {AlertColor} from "@mui/material/Alert/Alert";
 
 const AlertContext = createContext<any>(null);
 
 function AlertProvider({children}: any) {
   const [title, setTitle] = useState('');
+  const [severity, setSeverity] = useState<AlertColor>('success');
   const [isAlertOpen, setIsAlertOpen] = useState(false);
-  const openAlert = (title: string) => {
+  const openAlert = (title: string, severity?: AlertColor) => {
     setTitle(title);
+    severity && setSeverity(severity);
     setIsAlertOpen(true);
-    setTimeout(() => setIsAlertOpen(false), 1000);
+    setTimeout(() => {
+      setIsAlertOpen(false);
+      setSeverity("success");
+    }, 1000);
   };
   const closeAlert = () => {
     setIsAlertOpen(false);
@@ -19,11 +25,12 @@ function AlertProvider({children}: any) {
   const contextValue = useMemo(
     () => ({
       title,
+      severity,
       isAlertOpen,
       openAlert,
       closeAlert,
     }),
-    [title, isAlertOpen],
+    [title, severity, isAlertOpen],
   )
 
   return <AlertContext.Provider value={contextValue}>{children}</AlertContext.Provider>;
