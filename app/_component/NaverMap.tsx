fbd2d92393f3448319f73d40d29635d9396bb9b1
ehdgjsdl1 <ehdgjsdl1@naver.com>
2024-10-08 02:48:29 +0900

    Fix: 카카오 길찾기 링크 수정 // 카카오맵 어플 연동되도록

--- app/_component/NaverMap.tsx
+++ app/_component/NaverMap.tsx
@@ -49,7 +49,7 @@ export default function NaverMap() {
           네이버
         </a>
         <a className="btn btn-lg btn-dark" style={{width: '30%'}}
-           href={`https://map.kakao.com/link/to/${encNm},${lat},${lng}`}
+           href={`https://map.kakao.com/?map_type=TYPE_MAP&target=car&rt=,,514591,1111780&rt2=%ED%8A%B8%EB%9D%BC%EB%94%94%EB%85%B8%EC%9D%B4&rtIds=1726372096&rtTypes=PLACE`}
         >
           카카오
         </a>
@@ -62,39 +62,6 @@ export default function NaverMap() {
            }
         >
           티맵
-        </a>
-      </div>
-      <div className="d-flex justify-content-between">
-        <a className="btn btn-lg btn-dark w-auto"
-           href={`nmap://route/car?dname=${encNm}&dlat=${lat}&dlng=${lng}`}
-        >
-          네이버(모바일)
-        </a>
-        <a className="btn btn-lg btn-dark" style={{width: '30%'}}
-           href={
-             isMobile ?
-               `nmap://route/car?dname=${encNm}&dlat=${lat}&dlng=${lng}`
-               :
-               'https://map.naver.com/p/directions/-/14144926.3133202,4509290.5302953,%ED%8A%B8%EB%9D%BC%EB%94%94%EB%85%B8%EC%9D%B4,1297636555,PLACE_POI/-/car?c=15.00,0,0,0'
-           }
-        >
-          네이버(PC)
-        </a>
-      </div>
-      <div className="d-flex justify-content-between">
-        <a className="btn btn-lg btn-dark w-auto"
-           href={
-             `https://apis.openapi.sk.com/tmap/app/routes?appKey=${process.env.NEXT_PUBLIC_SK_APP_KEY}&name=${encNm}&lat=${lat}&lon=${lng}`
-           }
-        >
-          티맵(모바일)
-        </a>
-        <a className="btn btn-lg btn-dark w-auto"
-           href={
-             "https://poi.tmap.co.kr/sharing/positionSharing.do?contents=cGtleT04OTIzODEwMDAmcG9pSWQ9ODkyMzgxMCZuYXZTZXE9MCZ0eXBlPTImcG9pTmFtZT0lRUQlOEElQjglRUIlOUQlQkMlRUIlOTQlOTQlRUIlODUlQjglRUMlOUQlQjQmY2VudGVyWD00NTc0NDUxJmNlbnRlclk9MTM0OTk2NSZ0aW1lPTIwMjQlRUIlODUlODQrMTAlRUMlOUIlOTQrMDglRUMlOUQlQkMrMDElM0E1MSZ0ZWw9MDItNTY3LTk5MTYmYWRkcj0lRUMlODQlOUMlRUMlOUElQjgrJUVBJUIwJTk1JUVCJTgyJUE4JUVBJUI1JUFDKyVFQiU4RiU4NCVFQSVCMyVBMSVFQiVBMSU5Qzk5JUVBJUI4JUI4KzE2&tailParam=%7B%22reqType%22%3A%221100%22%2C%22reqMode%22%3A%221100%22%2C%22extra%22%3A%22112%22%7D"
-           }
-        >
-          티맵(PC)
         </a>
       </div>
     </>
--- app/api/is-mobile/route.ts
+++ app/api/is-mobile/route.ts
@@ -2,9 +2,7 @@ import {NextRequest, NextResponse} from "next/server";
 
 export async function GET(req: NextRequest) {
   const userAgent = req.headers.get("user-agent");
-  console.log("userAgent: " + userAgent);
   const isMobile = /Mobi|Android/i.test(userAgent || '');
-  console.log("isMobile: " + isMobile);
 
   return NextResponse.json(isMobile);
 }
