17da3671d8b13af5f31aa40560754fb06bc107db
ehdgjsdl1 <ehdgjsdl1@naver.com>
2024-10-08 03:29:24 +0900

    Feat: 길찾기 버튼 내 서비스별 로고 추가

--- app/_component/NaverMap.tsx
+++ app/_component/NaverMap.tsx
@@ -2,6 +2,7 @@
 
 import Script from "next/script";
 import {useEffect, useState} from "react";
+import Image from "next/image";
 
 
 const encNm = encodeURI("트라디노이");
@@ -37,8 +38,8 @@ export default function NaverMap() {
                 src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NCP_CLIENT_ID}`}/>
         <div id="map" style={{width: "100%", height: "400px", backgroundColor: "#E7E7E7"}}/>
       </div>
-      <div className="d-flex justify-content-between">
-        <a className="btn btn-lg btn-dark" style={{width: '30%'}}
+      <div className="d-flex justify-content-between bg-body-secondary">
+        <a className="btn btn-lg btn-dark d-flex align-items-center" style={{width: '32%'}}
            href={
              isMobile ?
                `nmap://route/car?dname=${encNm}&dlat=${lat}&dlng=${lng}`
@@ -46,14 +47,16 @@ export default function NaverMap() {
                'https://map.naver.com/p/directions/-/14144926.3133202,4509290.5302953,%ED%8A%B8%EB%9D%BC%EB%94%94%EB%85%B8%EC%9D%B4,1297636555,PLACE_POI/-/car?c=15.00,0,0,0'
            }
         >
-          네이버
+          <Image src={'/logo/navermap_icon.png'} alt={'네이버지도 로고'} width={26} height={26}/>
+          <span className="ms-auto" style={{lineHeight: '28px'}}>네이버</span>
         </a>
-        <a className="btn btn-lg btn-dark" style={{width: '30%'}}
+        <a className="btn btn-lg btn-dark d-flex align-items-center" style={{width: '32%'}}
            href={`https://map.kakao.com/?map_type=TYPE_MAP&target=car&rt=,,514591,1111780&rt2=%ED%8A%B8%EB%9D%BC%EB%94%94%EB%85%B8%EC%9D%B4&rtIds=1726372096&rtTypes=PLACE`}
         >
-          카카오
+          <Image src={'/logo/kakaomap_icon.png'} alt={'카카오맵 로고'} width={26} height={26}/>
+          <span className="ms-auto" style={{lineHeight: '28px'}}>카카오</span>
         </a>
-        <a className="btn btn-lg btn-dark" style={{width: '30%'}}
+        <a className="btn btn-lg btn-dark d-flex align-items-center" style={{width: '32%'}}
            href={
              isMobile ?
                `https://apis.openapi.sk.com/tmap/app/routes?appKey=${process.env.NEXT_PUBLIC_SK_APP_KEY}&name=${encNm}&lat=${lat}&lon=${lng}`
@@ -61,7 +64,8 @@ export default function NaverMap() {
                "https://poi.tmap.co.kr/sharing/positionSharing.do?contents=cGtleT04OTIzODEwMDAmcG9pSWQ9ODkyMzgxMCZuYXZTZXE9MCZ0eXBlPTImcG9pTmFtZT0lRUQlOEElQjglRUIlOUQlQkMlRUIlOTQlOTQlRUIlODUlQjglRUMlOUQlQjQmY2VudGVyWD00NTc0NDUxJmNlbnRlclk9MTM0OTk2NSZ0aW1lPTIwMjQlRUIlODUlODQrMTAlRUMlOUIlOTQrMDglRUMlOUQlQkMrMDElM0E1MSZ0ZWw9MDItNTY3LTk5MTYmYWRkcj0lRUMlODQlOUMlRUMlOUElQjgrJUVBJUIwJTk1JUVCJTgyJUE4JUVBJUI1JUFDKyVFQiU4RiU4NCVFQSVCMyVBMSVFQiVBMSU5Qzk5JUVBJUI4JUI4KzE2&tailParam=%7B%22reqType%22%3A%221100%22%2C%22reqMode%22%3A%221100%22%2C%22extra%22%3A%22112%22%7D"
            }
         >
-          티맵
+          <Image src={'/logo/tmap_icon.png'} alt={'티맵 로고'} width={26} height={26}/>
+          <span className="mx-auto" style={{lineHeight: '28px'}}>티 맵</span>
         </a>
       </div>
     </>
