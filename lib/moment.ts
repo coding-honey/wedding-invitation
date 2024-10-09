a8258cfa188fa62147ab5b4f59de0f58672f024f
ehdgjsdl1 <ehdgjsdl1@naver.com>
2024-10-09 21:24:03 +0900

    Fix: Calendar Header 내 요일 한글변환을 위해 moment.ts 내 locale/ko import 추가

--- lib/moment.ts
+++ lib/moment.ts
@@ -1,11 +1,10 @@
 import moment from "moment-timezone";
-// import 'moment/locale/ko'; // moment의 한글 로케일 import
+import 'moment/locale/ko'; // moment의 한글 로케일 import
 
 export const defaultTimezone = "Asia/Seoul";
 export const defaultLocale = "ko";
 
 moment.tz.setDefault(defaultTimezone);
-// moment.locale(defaultLocale);
 moment.updateLocale(defaultLocale, {
   week: {
     dow: 1
