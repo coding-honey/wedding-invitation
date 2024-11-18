e07f710f2b64388b230a87b5ba6ac0195f5d00b9
ehdgjsdl1 <ehdgjsdl1@naver.com>
2024-11-18 16:49:27 +0900

    Fix: D-Day 계산문구 client comp 로 전환

--- app/_component/calendar/CalendarDesc.tsx
+++ app/_component/calendar/CalendarDesc.tsx
@@ -5,25 +5,22 @@ import moment from "@/lib/moment";
 
 export default function CalendarDesc() {
   const [dDay, setDDay] = useState<number>(0);
-  const [today, setToday] = useState<string>("YYYY-MM-DD HH:mm:ss");
   useEffect(() => {
-    const weddingMomment = moment("2025-01-11");
+    const weddingMomment = moment();
     const today = moment();
     setDDay(weddingMomment.diff(today, "days"));
-    setToday(today.format("YYYY-MM-DD HH:mm:ss"));
   }, []);
 
   return (
     <div className={"fs-5"}>
-      <div>{today}</div>
       <span className={"fs-4"}>정동헌, 박선영 </span>
       님의 결혼식이
-      {(dDay && dDay > 0) ?
+      {(dDay > 0) ?
         <> <span style={{color: "blue"}}>{dDay} </span>
           일 남았어요.</> :
         (dDay === 0) ?
-          <> 오늘이에요! 🎉</> :
-          <> <span style={{color: "red"}}>{dDay && -dDay} </span>
+          <> <span className={"fs-4"}>오늘 </span>이에요! 🎉</> :
+          <> <span style={{color: "red"}}>{dDay} </span>
             일 지났어요.</>
       }
     </div>
