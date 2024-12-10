aba9c278e2d882ecd55c571fe0d305ce777f08f6
ehdgjsdl1 <ehdgjsdl1@naver.com>
2024-12-11 01:42:37 +0900

    Fix: 눈 내리는 효과 스크롤 다운시 따라오도록 style 추가

--- app/_component/SnowfallClient.tsx
+++ app/_component/SnowfallClient.tsx
@@ -5,6 +5,13 @@ import dynamic from "next/dynamic";
 const Snowfall = dynamic(() => import("react-snowfall"), {ssr: false});
 
 export default function SnowfallClient() {
-  return <Snowfall wind={[-0.5, 0.5]}/>
+  return <Snowfall
+    style={{
+      position: 'fixed',
+      width: '100vw',
+      height: '100vh',
+    }}
+    wind={[-0.5, 0.5]}
+  />
 }
 
