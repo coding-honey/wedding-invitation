e340317399d55e93ad6f0c6f9856513089e3479f
ehdgjsdl1 <ehdgjsdl1@naver.com>
2024-12-11 01:08:59 +0900

    Feat: Album 내 이미지 추가

--- app/_component/ImageSlider.tsx
+++ app/_component/ImageSlider.tsx
@@ -6,8 +6,13 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import {Autoplay, Pagination} from 'swiper/modules';
 import Image from "next/image";
-import album1Image from "@/public/Image/album1.png";
-import album2Image from "@/public/Image/album2.png";
+import album1Image from "@/public/Image/album/album-img-1.jpeg";
+import album2Image from "@/public/Image/album/album-img-2.jpeg";
+import album3Image from "@/public/Image/album/album-img-3.jpeg";
+import album4Image from "@/public/Image/album/album-img-4.jpeg";
+import album5Image from "@/public/Image/album/album-img-5.jpeg";
+import album6Image from "@/public/Image/album/album-img-6.jpeg";
+import album7Image from "@/public/Image/album/album-img-7.jpeg";
 
 export default function ImageSlider() {
   return (
@@ -28,16 +33,54 @@ export default function ImageSlider() {
             width={408}
             height={230}
             alt="Album slide image 1"/>
-          {/*<img src="/image/album1.png" alt="Album Image 1"/>*/}
         </SwiperSlide>
-        <SwiperSlide>
+        <SwiperSlide className="mt-auto">
           <Image
             className="img-fluid"
             src={album2Image}
             width={408}
             height={230}
             alt="Album slide image 2"/>
-          {/*<img src="/image/album2.png" alt="Album Image 2"/>*/}
+        </SwiperSlide>
+        <SwiperSlide className="mt-auto">
+          <Image
+            className="img-fluid"
+            src={album3Image}
+            width={408}
+            height={230}
+            alt="Album slide image 3"/>
+        </SwiperSlide>
+        <SwiperSlide className="mt-auto">
+          <Image
+            className="img-fluid"
+            src={album4Image}
+            width={408}
+            height={230}
+            alt="Album slide image 4"/>
+        </SwiperSlide>
+        <SwiperSlide className="mt-auto">
+          <Image
+            className="img-fluid"
+            src={album5Image}
+            width={408}
+            height={230}
+            alt="Album slide image 5"/>
+        </SwiperSlide>
+        <SwiperSlide className="mt-auto">
+          <Image
+            className="img-fluid"
+            src={album6Image}
+            width={408}
+            height={230}
+            alt="Album slide image 6"/>
+        </SwiperSlide>
+        <SwiperSlide className="mt-auto">
+          <Image
+            className="img-fluid"
+            src={album7Image}
+            width={408}
+            height={230}
+            alt="Album slide image 7"/>
         </SwiperSlide>
       </Swiper>
     </>
