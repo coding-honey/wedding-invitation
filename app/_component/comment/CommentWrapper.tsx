5f5f4e9a101b1ce819a8c3670ef42d37a4f3fb30
ehdgjsdl1 <ehdgjsdl1@naver.com>
2024-12-11 20:27:46 +0900

    Revert "Test: userAgent 확인"
    This reverts commit 24c6a29646aea6a2a19e44b217721c630f598d3a.

--- app/_component/comment/CommentWrapper.tsx
+++ app/_component/comment/CommentWrapper.tsx
@@ -1,18 +1,12 @@
 "use client";
 
 import CommentC from "@/types/comment";
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import CommentForm from "@/app/_component/comment/CommentForm";
 import CommentList from "@/app/_component/comment/CommentList";
 
 export default function CommentWrapper() {
   const [comments, setComments] = useState<CommentC[]>([]);
-
-  const [userAgent, setUserAgent] = useState('');
-  useEffect(() => {
-    const userAgent = typeof window !== 'undefined' ? navigator.userAgent : '';
-    setUserAgent(userAgent);
-  }, [])
 
   return (
     <>
@@ -22,7 +16,6 @@ export default function CommentWrapper() {
       <div className="section">
         <CommentList comments={comments} setComments={setComments}/>
       </div>
-      <div>{userAgent}</div>
     </>
   );
 }
