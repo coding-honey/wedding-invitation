8be3ad9935f74b83f46f39d849cd0cba99e04e87
ehdgjsdl1 <ehdgjsdl1@naver.com>
2024-12-25 01:53:01 +0900

    Feat: 오시는 길 영역 내 약도 이미지 추가

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import AttendanceWrapper from "@/app/_component/AttendanceWrapper";
 import mainImg1 from "@/public/Image/main-img-1.jpeg";
 import mainImg2 from "@/public/Image/main-img-2.jpeg";
 import mainImg3 from "@/public/Image/main-img-3.png";
+import sketchMapImg from "@/public/Image/sketch_map.png";
 import CommentWrapper from "@/app/_component/comment/CommentWrapper";
 import ContactWrapper from "@/app/_component/contact/ContactWrapper";
 import BankAccordionWrapper from "@/app/_component/accordion/BankAccordionWrapper";
@@ -38,8 +39,7 @@ export default function Home() {
           src={mainImg1}
           width={430}
           height={645}
-          alt="main image 1">
-        </Image>
+          alt="main image 1"/>
       </div>
       <div className="section">
         <p>
@@ -59,8 +59,7 @@ export default function Home() {
           src={mainImg2}
           width={429}
           height={430}
-          alt="main image 2">
-        </Image>
+          alt="main image 2"/>
       </div>
       <div className="section">
         <div className="row">
@@ -117,35 +116,40 @@ export default function Home() {
       <hr/>
       <div className="section">
         <p className="fs-3 fw-bold">오시는 길</p>
-        <p className="mb-0">
-          트라디노이 6층 하우스웨딩홀<br/>
-        </p>
-        <p className="d-flex justify-content-center">
+        * 트라디노이 6층 하우스웨딩홀<br/>
+        <div className="d-flex justify-content-center" style={{marginBottom: '2rem'}}>
           주소: 서울 강남구 도곡로99길 16 <CopyButton text={'서울 강남구 도곡로99길 16'} iconSize={'lg'} style={{marginLeft: '0.5rem'}}/>
-        </p>
+        </div>
         <NaverMap/>
         <MapButtonWrapper/>
+      </div>
+      <div className="section">
+        <Image
+          className="img-fluid"
+          src={sketchMapImg}
+          width={439}
+          height={645}
+          alt="트라디노이 약도"/>
       </div>
       <div className="section text-start card">
         <div className="card-body">
           <p className="fs-4 fw-bold">지하철</p>
           <p>
-            [2호선] 삼성역 3번출구 도보 14분<br/>
+            [2호선] 삼성역 3번출구 도보 13분<br/>
             [3호선] 학여울역 1번출구 도보 14분
           </p>
           <p className="fs-4 fw-bold">버스</p>
           <p>
-            마을버스 강남 01, 강남 06<br/>
-            401, 4319, 4419, 500-2, 9507, 9607, 11-3, 917
+            (마을버스) 강남 01, 강남 06<br/>
+            (간선) 401 / (지선) 4318, 4319, 4419<br/>
+            (직행) 500-2, 9507, 9607 / (일반) 11-3, 917
           </p>
           <p className="fs-4 fw-bold">주차정보</p>
-          <p className="mb-0">
-            * 대치2동문화센터 공영주차장입구<br/>
-          </p>
-          <p className="d-flex justify-content-start">
+          * 대치2동문화센터 공영주차장입구<br/>
+          <div className="d-flex justify-content-start">
             주소: 서울 강남구 영동대로65길 24 <CopyButton text={'서울 강남구 영동대로65길 24'} iconSize={'lg'}
                                               style={{marginLeft: '0.5rem'}}/>
-          </p>
+          </div>
         </div>
       </div>
       <div className="section">
@@ -154,8 +158,7 @@ export default function Home() {
           src={mainImg3}
           width={439}
           height={645}
-          alt="main image 3">
-        </Image>
+          alt="main image 3"/>
       </div>
       <div className="section">
         <p className="fs-3 fw-bold">마음 전하실 곳</p>
